a9373bdaa60e5f55df783a6b5619e6450a186b37
alejandrosanchezr2023 <alejosanchez82@icloud.com>
2023-07-12 21:44:43 -0500

    directiva ngFor para desplegar datos de una lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Saludo } from './components/saludo.component';
+import { DespedidaComponent } from './components/despedida/despedida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Saludo,
+    DespedidaComponent
   ],
   imports: [
     BrowserModule
