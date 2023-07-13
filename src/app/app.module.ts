b7ffd55647f7bd1e22239695191c3d5f1c7953b0
alejandrosanchezr2023 <alejosanchez82@icloud.com>
2023-07-13 18:55:45 -0500

    ng g c components/login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports:[
     BrowserModule
