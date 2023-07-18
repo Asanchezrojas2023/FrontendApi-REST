bfa9b925dbdfc7e458b36450d51304a32b683f8b
alejandrosanchezr2023 <alejosanchez82@icloud.com>
2023-07-17 21:47:17 -0500

    dashboard.component: Define estructura HTML & agregamos metodo logout()

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
         component: DashboardComponent
       },
       {
-        path: '**',
+        path: '**', 
         redirectTo: ''
       }
     ]     // http://localhost:4200/dashboard
