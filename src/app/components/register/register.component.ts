09b27bed72b7279a4e70ad26ca21c19d467b29d9
alejandrosanchezr2023 <alejosanchez82@icloud.com>
2023-07-17 20:36:35 -0500

    login.component: creamos validators

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+// A tributos
 
+  registerForm: FormGroup = this.fb.group({
+    nombre:[
+      '', // valor por defecto para campo
+      [   // validaciones
+        Validators.required
+      ]
+    ],
+    email: [
+      '',  // valor por defecto para el campo
+      [  // validaciones
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',  // valor por defecto para el campo
+      [  // validaciones
+        Validators.required,
+        Validators.minLength( 8 ),
+      ]
+    ]
+  })
+
+  // inyeccion de dependencia
+ constructor ( private fb: FormBuilder)  {}
+ // Metodo: Register
+ register(){
+  console.group('registerForm');
+  console.info( this.registerForm.value);
+  console.info( this.registerForm.valid);
+  console.groupEnd();
+  
+ }
 }
